37ae5d78f0c8ab18581644730600ed07f7bdac56
Alanwake <wjy1063896722@outlook.com>
2018-05-15 08:24:16 +0800

    之前的版本不能用

--- test2-7.c
+++ test2-7.c
@@ -1,16 +1,16 @@
-#include <stdio.h>
-int main(){
-    int x=0,i;
-    while(1){
-        for(i=1;i<10;i++){
-            x=(x/2)-1;
-        }
-        
-		if(x==1){
-			break;
-		}
-        x++;
-    }
-    printf("X=%d\n",x);
-    return 0;
+#include<stdio.h>  
+#include <stdlib.h>  
+int main()  
+{  
+    int peach = 0;//桃子总数  
+    int rest = 1;//第10 天只剩一个桃子  
+    int day = 0;  
+    for(day = 9; day > 0; day--)  
+    {  
+        peach = (rest + 1) * 2;//每天的桃子总数是后一天剩余桃子加1乘2  
+        rest = peach;  
+    }  
+    printf("猴子第一天一共摘了%d个桃子\n",peach);  
+    system("pause");  
+    return 0;  
 }
