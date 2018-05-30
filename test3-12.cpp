c892e824cda7270b99b38542ad6ceb4f37ec4f45
xjx00 <1504635269@qq.com>
2018-05-30 17:27:05 +0800

    实验报告三第十二题

---
+++ test3-12.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int prime(int n)
+
+{int i;
+
+ for(i=2;i*i<=n;i++)
+
+   if(n%i==0)return 0;
+
+ return 1;  
+
+}
+
+int main()
+
+{int i,n=0;
+
+ for(i=2;i<300;i++)
+
+   if(prime(i))
+
+{printf("%5d",i);
+
+if(++n%9==0)printf("\n");
+
+}
+
+ return 0;
+
+}
